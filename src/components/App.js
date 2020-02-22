c67b168ce7c98d0f203335278d64a32af7a8eb9d
Mateusz <matstanczak@gmail.com>
2020-02-22 19:40:55 +0000

    Search Bar added

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import SearchBar from './SearchBar';
 
 class App extends React.Component {
     render() {
         return (
             <div>
-                App Component
+                <SearchBar />
             </div>
-        )
+        );
     }
 };
 
